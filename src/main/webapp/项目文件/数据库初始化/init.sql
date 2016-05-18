5117d1321c7494f9dabe30e7ca2757e37ae6a50f
zhangshan <zhangshan@utry.cn>
2016-05-18 16:13:00 +0800

    上传数据库初始化脚本 Signed-off-by: zhangshan <zhangshan@utry.cn>

---
+++ src/main/webapp/项目文件/数据库初始化/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `user` (
+`id`  int(11) NOT NULL AUTO_INCREMENT COMMENT '用户编号' ,
+`name`  varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名称' ,
+`age`  int(3) NOT NULL COMMENT '用户年龄' ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+COMMENT='用户表'
+AUTO_INCREMENT=126
+ROW_FORMAT=COMPACT
+;
+
